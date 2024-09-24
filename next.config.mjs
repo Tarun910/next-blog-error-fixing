eb5f31cf1a450a4281437e8057b8edde0f40acb1
Tarun Namdev <namdevtarun910@gmail.com>
2024-09-24 14:32:38 +0530

    now committed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["images.pexels.com"], // Add the Pexels domain here
+  },
+};
 
-export default nextConfig;
+export default nextConfig; // Use export default instead of module.exports
